2f5b46a675267e8c20b53e249a659567671d8b32
nnminhtan <93578088+nnminhtan@users.noreply.github.com>
2023-04-20 22:19:32 +0700

    Add files via upload
    add all files (Not fully work yet)

---
+++ hanghoa.java
@@ -0,0 +1,27 @@
+package OOP.tuan1;
+
+import java.util.Scanner;
+
+public class hanghoa {
+    protected String MaHang, TenHang, NSX;
+    protected double Gia;
+    protected void input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ma hang: ");
+        MaHang=sc.nextLine();
+        System.out.print("Nhap ten hang: ");
+        TenHang=sc.nextLine();
+        System.out.print("Nhap NSX: ");
+        NSX=sc.nextLine();
+        System.out.print("Nhap gia: ");
+        Gia=sc.nextDouble();
+        sc.close();
+    }
+    protected void output(){
+        System.out.print(MaHang + " " + TenHang + " " + NSX + " " + Gia);
+    }
+    protected double Tinh_gia_ban(){
+        return Gia;
+    }
+
+}
---
+++ main.java
@@ -0,0 +1,18 @@
+package OOP.tuan1;
+
+public class main {
+    public static void main(String[] args){
+        // hanghoa hh = new hanghoa();
+        // hh.input();
+        // hh.output();
+        // hangdienmay hdm = new hangdienmay();
+        // hdm.input();
+        // hdm.output();
+        hangss hss = new hangss();
+        hss.input();
+        hss.output();
+        // hangthucpham htp = new hangthucpham();
+        // htp.input();
+        // htp.output();
+    }
+}
