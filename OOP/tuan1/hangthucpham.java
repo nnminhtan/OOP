b025d53378b23c6847a7dd9d40f4171b96b6c4c7
nnminhtan <93578088+nnminhtan@users.noreply.github.com>
2023-04-21 21:52:06 +0700

    Sort em in a folder

---
+++ OOP/tuan1/hangthucpham.java
@@ -0,0 +1,27 @@
+package OOP.tuan1;
+
+import java.util.Scanner;
+
+public class hangthucpham extends hanghoa{
+    protected String MFG, EXP;
+    protected int EXPD;
+
+    public void input(){
+        super.input();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ngay sx: ");
+        MFG=sc.nextLine();
+        System.out.print("Nhap so ngay het han: ");
+        EXPD=sc.nextInt();
+        EXP = MFG + EXPD;
+    }
+
+    public void output(){
+        super.output();
+        System.out.print(" "+ MFG + " " + EXP);
+    }
+    public double Tinh_gia_ban(){
+        super.Tinh_gia_ban();
+        return Gia;
+    }
+}
